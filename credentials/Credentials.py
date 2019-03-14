ab762a74226f32c25d9495f81ccce60265d09b87
LauraKag <laura.kageneck@student.ie.edu>
2019-03-14 20:26:10 +0100

    Changes

---
+++ credentials/Credentials.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 20:24:42 2019
+
+@author: laura
+"""
+
---
+++ credentials/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 20:22:30 2019
+
+@author: laura
+"""
+
