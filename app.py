89757f329e7e082b00c871fb1294866ec0c930ec
LauraKag <laura.kageneck@student.ie.edu>
2019-03-22 19:06:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ import pandas as pd
 
 conn = pg.connect("postgres://svaoxmufmplmsu:8a40bddf7d6d4bd1bec788f91feacdbdcb0404d83aeda1ac6b40410340eea95b@ec2-75-101-131-79.compute-1.amazonaws.com:5432/d62btbprhf4ja3")
 
-a = pd.read_sql_query('select "Incident ID" from "JanJunData" limit 1;', conn)
+a = pd.read_sql_query('select * from "JanJunData" limit 1;', conn)
 
 app = dash.Dash(__name__)
 server = app.server
 
 
 app.layout = html.Div([
-    html.H2(a),
+    html.H2('Hello World'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
