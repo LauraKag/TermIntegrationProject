99637a0309af08426469f85de35d7eefeb7b220f
LauraKag <laura.kageneck@student.ie.edu>
2019-03-22 19:01:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,22 @@ Created on Thu Mar 14 19:56:15 2019
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import psycopg2 as pg
+import pandas as pd 
 
 
+
+
+conn = pg.connect("postgres://svaoxmufmplmsu:8a40bddf7d6d4bd1bec788f91feacdbdcb0404d83aeda1ac6b40410340eea95b@ec2-75-101-131-79.compute-1.amazonaws.com:5432/d62btbprhf4ja3")
+
+a = pd.read_sql_query('select "Incident ID" from "JanJunData" limit 1;', conn)
 
 app = dash.Dash(__name__)
 server = app.server
 
 
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2(a),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
