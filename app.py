fe8b367ed8f147677f02329721c79f4ee650f79b
LauraKag <laura.kageneck@student.ie.edu>
2019-03-23 11:00:51 +0100

    Changes

--- app.py
+++ app.py
@@ -10,36 +10,78 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import psycopg2 as pg
-import pandas as pd 
+import pandas as pd
 
-
-
+from dash.dependencies import Input, Output
 
 conn = pg.connect("postgres://svaoxmufmplmsu:8a40bddf7d6d4bd1bec788f91feacdbdcb0404d83aeda1ac6b40410340eea95b@ec2-75-101-131-79.compute-1.amazonaws.com:5432/d62btbprhf4ja3")
 
-a = pd.read_sql_query('select * from "JanJunData" limit 1;', conn)
+df = pd.read_sql_query('select * from "JanJunData" limit 10;', conn)
 
-app = dash.Dash(__name__)
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+
+
+#%% Set Up Layout 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-b=a["Incident ID"]
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.H2(b),
-
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    html.H1('IBERIA'),
+    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
+        dcc.Tab(label='Incidences', value='tab-1-example'),
+        dcc.Tab(label='Applications', value='tab-2-example'),
+        dcc.Tab(label='Services', value='tab-3-example'),
+    ]),
+    html.Div(id='tabs-content-example')
 ])
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+@app.callback(Output('tabs-content-example', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1-example':
+        return html.Div([
+            html.H3('Tab content 1'),
+            dcc.Graph(
+                id='graph-1-tabs',
+                figure={
+                    'data': [{
+                        'x': [1, 2, 3],
+                        'y': [3, 1, 2],
+                        'type': 'bar'
+                    }]
+                }
+            )
+        ])
+    elif tab == 'tab-2-example':
+        return html.Div([
+            html.H3('Tab content 2'),
+            generate_table(df)
+            ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'), [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    elif tab== 'tab-3-example':
+        return html.Div([
+            html.H3('Tab content 2'),
+            generate_table(df)
+        ])
+
 
 
 if __name__ == '__main__':
