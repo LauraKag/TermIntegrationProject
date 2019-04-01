cebf1d852f74d28c67d55cb61a35b460118f22a0
LauraKag <laura.kageneck@student.ie.edu>
2019-04-02 00:27:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -266,28 +266,28 @@ tab1_layout = html.Div([
 
     ],className='row'),
 
-    html.Div([
-        dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
+    #html.Div([
+       # dcc.Upload(
+      #  id='upload-data',
+      #  children=html.Div([
+      #      'Drag and Drop or ',
+      #      html.A('Select Files')
+      #  ]),
+      #  style={
+      #      'width': '100%',
+      #      'height': '60px',
+      #      'lineHeight': '60px',
+      #      'borderWidth': '1px',
+      #      'borderStyle': 'dashed',
+      #      'borderRadius': '5px',
+      #      'textAlign': 'center',
+      #      'margin': '10px'
+     #   },
         # Allow multiple files to be uploaded
         #multiple=True
-    ),
-    html.Div(id='output-data-upload'),
-],className="row")
+   # ),
+  #  html.Div(id='output-data-upload'),
+#],className="row")
 
         ])
 
@@ -1273,10 +1273,6 @@ def find_numbers_of_CH_Service(value):
                 'xaxis': {'tickangle':"-90"}
             }
         })
-
-
-
-
 
 
     
